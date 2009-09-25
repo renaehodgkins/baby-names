0d82902c2db8b57ac7c104c65174c2a11afce436
Renae Bair <renaebair@gmail.com>
2009-09-24 21:31:52 -0400

    Added comments for names

--- app.rb
+++ app.rb
@@ -76,3 +76,21 @@ post '/:id/vote/:rank' do
   redirect '/'   
 end
 
+get '/:id/comments' do
+  @name = Name.get(params[:id])
+  erb :comments
+end
+
+
+# create comment
+post '/:id/comments' do
+  @name = Name.get(params[:id])
+  @comment = @name.comments.new(:body => params[:comment_body])
+  if @comment.save
+    @message = "Commnet saved"
+  else
+    @message = "Unable to save comment"
+  end
+  redirect "/#{@name.id}/comments"
+end
+
--- models/name.rb
+++ models/name.rb
@@ -7,6 +7,7 @@ class Name
   property :updated_at, DateTime
   
   has n, :votes
+  has n, :comments
   
   validates_present :gender, :name
   use Rack::Session::Cookie, :secret => 'A1 sauce 1s so good you should use 1t on a11 yr st34ksssss'
