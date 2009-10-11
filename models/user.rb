05177974b1f0866e7c2417a456c1e6f357b37280
Renae Bair <renaebair@gmail.com>
2009-10-10 21:20:51 -0400

    Added heading for main page

--- models/user.rb
+++ models/user.rb
@@ -6,3 +6,4 @@ class User
   validates_is_unique :email, :url
 end
 
+#user must be able to sign up for new account, linked to email address/password, have their page generate a unique url to give to friends and family, be able to delete names or comments, display winning name, choose a gender or leave it as unknown, add another baby page (craete another unique url for subsequent babies). 
