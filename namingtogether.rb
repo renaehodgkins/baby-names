56eff3b6d76871780912c140dbf7378b387a2455
Adam Bair <adambair@gmail.com>
2009-12-23 13:54:52 -0500

    Remove unnecessary utf8 declarations

--- namingtogether.rb
+++ namingtogether.rb
@@ -67,12 +67,10 @@ before do
 end
 
 get '/stylesheets/stylesheet.css' do
-  content_type 'text/css', :charset => 'utf-8'
   sass :stylesheet
 end
 
 get '/stylesheets/reset.css' do
-  content_type 'text/css', :charset => 'utf-8'
   sass :reset
 end
 
